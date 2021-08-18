a047ce28b487c1143baaa1c2068a7fbb28677b6c
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-18 22:19:03 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './templates/cards.hbs';
+
+import apiService from './js/apiService.js';
+import refs from './js/refs.js';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  form: document.getElementById('search-form'),
+  list: document.querySelector('.gallery'),
+  searchBtn: document.querySelector('.searchBtn'),
+};
+
+export default refs;
